45065ceceb35dfd4a31cb0020b9f10020625fc2d
Demetri Van Sickle <demetriv.s.7@gmail.com>
2019-12-09 12:29:01 -0800

    Changed the way the music was loaded to work with iOS devices

--- public/index.php
+++ public/index.php
@@ -15,34 +15,41 @@ body{
 }
 </style>
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
-</head>
-<body>
-<!----JS FUNCTIONS---->
+<script src="https://code.createjs.com/1.0.0/createjs.min.js"></script>
 <script>
+    var soundID = "carolBells";
 
-function showStart() 
-{
-    $.post("_runShow.php", {bed: "essr"}, 
-        function(test)
+    function loadSound () {
+        createjs.Sound.registerSound("carolOfTheBells.mp3", soundID);
+    }
+
+    function playSound () {
+        createjs.Sound.play(soundID);
+    }
+
+    function showStart() 
+    {
+        $.post("_runShow.php", {bed: "essr"}, 
+            function(test)
             {
                 //alert(test);
                 if(test == 1)
                 {
-                   // alert("Playing");
+                    // alert("Playing");
 
-                    var audio = document.getElementById("music");
-                    audio.play();
+                    //var audio = document.getElementById("music");
+                    //audio.play();
+                    playSound();
                 }
                 else
                 {
                     alert("Show already playing!");
                 }
             } );  
-}
+    }
 </script>
-
-
-
+</head>
+<body onload="loadSound();">
 
 <p>Welcome</p>
         
@@ -58,32 +65,30 @@ document.getElementById("submitBtn").addEventListener("click", showStart);
 
 
 
-<audio id="music" src="carolOfTheBells.mp3" preload="auto" ></audio>
+<!--<audio id="music" src="carolOfTheBells.mp3" preload="auto" ></audio>--->
 
 <script>
-function sleep(milliseconds) {
+/*function sleep(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
     if ((new Date().getTime() - start) > milliseconds){
       break;
     }
   }
-}
+}*/
 
 
 
-var musicLoaded = false;
+/*var musicLoaded = false;
 
 
-/*document.getElementById("music").onplaying =  mLoaded; 
-    mLoaded()
+document.getElementById("music").onplaying =  mLoaded()
     {
         musicLoaded = true; 
         alert("loaded");
-    }
+    };
  document.getElementById("music").addEventListener("onloadstart", 
-    mLoading(){alert("loading");});
-*/
+    mLoading(){alert("loading");});*/
 
 </script>
 
